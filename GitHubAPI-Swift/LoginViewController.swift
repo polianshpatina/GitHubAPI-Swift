7eece69a0a0a9e208c64519bb7a873cf18d25a60
Polian <polian.shpatina@aisoftwarellc.com>
2020-11-02 22:53:05 -0500

    Authentication is implemented

--- GitHubAPI-Swift/LoginViewController.swift
+++ GitHubAPI-Swift/LoginViewController.swift
@@ -22,7 +22,40 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func loginPressed(_ sender: UIButton) {
-        if usernameTextfield.text == "" {
+        if usernameTextfield.text != "" {
+            let authentication = BasicAuthentication(username: usernameTextfield.text!, password: "Samsunggalaxyj7!")
+            UserAPI(authentication: authentication).getUser { (response, error) in
+                if let response = response {
+                    if response.id != nil {
+                        DispatchQueue.main.async{
+                            self.performSegue(withIdentifier: "LoginToRepositories", sender: self)
+                           }
+                    }
+                    else {
+                        DispatchQueue.main.async{
+                         // create the alert
+                         let alert = UIAlertController(title: "Alert", message: "Username or Password is incorrect!", preferredStyle: UIAlertController.Style.alert)
+                         
+                         // add an action (button)
+                         alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+                         
+                         // show the alert
+                         self.present(alert, animated: true, completion: nil)
+                        }                        
+                    }
+                } else {
+                    // create the alert
+                    let alert = UIAlertController(title: "Alert", message: error.debugDescription, preferredStyle: UIAlertController.Style.alert)
+                    
+                    // add an action (button)
+                    alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+                    
+                    // show the alert
+                    self.present(alert, animated: true, completion: nil)
+                }
+            }
+        }
+        else {
             // create the alert
             let alert = UIAlertController(title: "Alert", message: "Username can not be empty!", preferredStyle: UIAlertController.Style.alert)
             
@@ -35,7 +68,9 @@ class LoginViewController: UIViewController {
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let vc = segue.destination as? RepositoriesViewController {
+        if segue.identifier == "LoginToRepositories"
+        {
+            let vc = segue.destination as! RepositoriesViewController
             vc.username = usernameTextfield.text!
         }
     }
